091dfcc0898ae6d6fd0703a37defc0e2a9a5d062
ColeWhaley1 <colewhaley1@gmail.com>
2024-06-29 15:21:11 -0400

    fix firebase set up options

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:workout_app/firebase_options.dart';
 import 'package:workout_app/pages/schedule/schedule.dart';
 import 'package:workout_app/pages/workouts/workouts.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
