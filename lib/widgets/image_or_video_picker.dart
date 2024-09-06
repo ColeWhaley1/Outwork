d4ffbac929dfeec5dfb53ffcc9449604660e7746
ColeWhaley1 <colewhaley1@gmail.com>
2024-09-06 16:39:16 -0400

    get video to play

--- lib/widgets/image_or_video_picker.dart
+++ lib/widgets/image_or_video_picker.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class ImageOrVideoPicker extends StatefulWidget {
   const ImageOrVideoPicker({super.key, required this.onChanged});
@@ -16,17 +17,21 @@ class _ImageOrVideoPickerState extends State<ImageOrVideoPicker> {
   final ImagePicker _picker = ImagePicker();
   XFile? _image;
   XFile? _video;
+  VideoPlayerController? _videoController;
+
   final List<String> _links = [];
 
   Future<void> _pickImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
     setState(() {
+      _videoController?.dispose();
+      
       _image = image;
       _video = null;
 
       //add path to links for form submission
       final imageLink = _image?.path;
-      if(imageLink is String){
+      if (imageLink is String) {
         _links.add(imageLink);
         setState(() {
           widget.onChanged(_links);
@@ -36,20 +41,27 @@ class _ImageOrVideoPickerState extends State<ImageOrVideoPicker> {
   }
 
   Future<void> _pickVideo() async {
+    _videoController?.dispose();
     final XFile? video = await _picker.pickVideo(source: ImageSource.gallery);
-    setState(() {
-      _video = video;
-      _image = null;
+    if (video != null) {
+      setState(() {
+        _video = video;
+        _image = null;
 
-      //add path to links for form submission
-      final videoLink = _video?.path;
-      if(videoLink is String){
-        _links.add(videoLink);
-        setState(() {
-          widget.onChanged(_links);
+        _videoController = VideoPlayerController.file(File(video.path))..initialize().then((_) {
+          setState(() {}); // ensures first frame is displayed
         });
-      }
-    });
+
+        //add path to links for form submission
+        final videoLink = _video?.path;
+        if (videoLink is String) {
+          _links.add(videoLink);
+          setState(() {
+            widget.onChanged(_links);
+          });
+        }
+      });
+    }
   }
 
   @override
@@ -109,18 +121,30 @@ class _ImageOrVideoPickerState extends State<ImageOrVideoPicker> {
               ],
             ),
           ],
-          if (_video != null) ...[
-            Container(
-              width: 300,
-              height: 200,
-              color: Colors.black,
-              child: Center(
-                child: Text(
-                  'Video Selected: ${_video!.name}',
-                  style: const TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
+          if (_video != null && _videoController != null) ...[
+            _videoController!.value.isInitialized
+                ? Container(
+                  constraints: BoxConstraints(
+                    maxWidth: MediaQuery.of(context).size.width - 16,
+                    maxHeight: 300),
+                  child: AspectRatio(
+                      aspectRatio: _videoController!.value.aspectRatio,
+                      child: VideoPlayer(_videoController!)),
+                )
+                : const CircularProgressIndicator(),
+            const SizedBox(height: 10),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _videoController!.value.isPlaying ? 
+                    _videoController!.pause() :
+                    _videoController!.play();
+                });
+              },
+              child: Icon(
+                _videoController!.value.isPlaying ? 
+                  Icons.pause :
+                  Icons.play_arrow
               ),
             )
           ],
